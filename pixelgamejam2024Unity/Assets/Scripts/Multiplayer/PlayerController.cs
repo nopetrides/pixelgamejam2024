57e49d2cae9297bdff5b42a95b6bc7193168ec0a
André Borja <andre.borja.miranda@gmail.com>
2024-05-11 21:34:29 -0700

    controller

--- pixelgamejam2024Unity/Assets/Scripts/Multiplayer/PlayerController.cs
+++ pixelgamejam2024Unity/Assets/Scripts/Multiplayer/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,10 +9,13 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private PlayerCharacterSO _playerSO;
+    private float _moveSpeed;
     private float _maxSpeed;
     private float _carryCapacity;
+    private float _currentCarriedWeight;
     private int _health;
     private int _attack;
+    private bool _attackCooldown;
     [SerializeField]
     private float _moveForce = 1f;
     private Vector3 _move;
@@ -29,6 +33,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private Camera _cam;
     private Transform _camTransform;
+
+    [SerializeField]
+    private CapsuleCollider _pickupZone;
+    [SerializeField]
+    private GameObject _attackZone;
+
+    
     
 
     private void Awake()
@@ -83,12 +94,12 @@ public class PlayerController : MonoBehaviour
     
     private void OnPickupPressed(InputAction.CallbackContext obj)
     {
-        
+        Grab();
     }
 
     private void OnAttackPressed(InputAction.CallbackContext obj)
     {
-        
+        if(!_attackCooldown) Attack();
     }
     
     #endregion
@@ -104,8 +115,8 @@ public class PlayerController : MonoBehaviour
 
         Vector3 horizontalVelocity = _rb.velocity;
         horizontalVelocity.y = 0f;
-        if (horizontalVelocity.sqrMagnitude > _maxSpeed * _maxSpeed)
-            _rb.velocity = horizontalVelocity.normalized * _maxSpeed;
+        if (horizontalVelocity.sqrMagnitude > _moveSpeed * _moveSpeed)
+            _rb.velocity = horizontalVelocity.normalized * _moveSpeed;
         
         LookAt();
     }
@@ -160,5 +171,58 @@ public class PlayerController : MonoBehaviour
     {
         _facingLeft = !_facingLeft;
         _spriteRenderer.flipX = !_spriteRenderer.flipX;
+    }
+
+    private void Grab()
+    {
+        Collider2D[] collider2Ds = Physics2D.OverlapCircleAll(transform.position, _pickupZone.radius);
+        foreach (Collider2D collider2D in collider2Ds)
+        {
+            if (collider2D.GetComponent<Pickup>() != null)
+            {
+                if (_currentCarriedWeight >= _carryCapacity)
+                {
+                    //Print fail message
+                    return;
+                }
+                collider2D.GetComponent<Pickup>()?.Interact();
+                //Pickup object; add weight
+                SetMaxSpeed();
+                //FixedJoint2D fj = transform.gameObject.AddComponent(typeof(FixedJoint2D)) as FixedJoint2D;
+            }
+
+            if (collider2D.GetComponent<Conveyour>() != null && _currentCarriedWeight > 0)
+            {
+                //Unload
+                SetMaxSpeed();
+            }
+        }
+    }
+
+    private void SetMaxSpeed()
+    {
+        if (_currentCarriedWeight <= _carryCapacity * 0.25f)
+        {
+            _moveSpeed = _maxSpeed; 
+        }else if (_currentCarriedWeight <= _carryCapacity * 0.5f)
+        {
+            _moveSpeed = _maxSpeed * 0.75f;
+        }else if (_currentCarriedWeight <= _carryCapacity * 0.75f)
+        {
+            _moveSpeed = _maxSpeed * 0.5f;
+        }
+        else
+        {
+            _moveSpeed = _maxSpeed * 0.25f;
+        }
+    }
+
+    private async void Attack()
+    {
+        _attackZone.SetActive(true);
+        _attackCooldown = true;
+        await Task.Delay(250);
+        _attackZone.SetActive(false);
+        _attackCooldown = false;
     }
 }
