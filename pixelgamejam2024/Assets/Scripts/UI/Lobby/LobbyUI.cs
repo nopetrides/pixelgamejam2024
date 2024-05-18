89c8db1ab8a998c9a2da9987c1d90a52fc7a16cd
Noah Petrides <nopetrides@gmail.com>
2024-05-17 23:19:40 -0700

    Happy treasure days

--- pixelgamejam2024/Assets/Scripts/UI/Lobby/LobbyUI.cs
+++ pixelgamejam2024/Assets/Scripts/UI/Lobby/LobbyUI.cs
@@ -109,13 +109,13 @@ public class LobbyUI : MonoBehaviour
         Debug.Log("Start Game Pressed!");
         if (!PlayroomKit.IsRunningInBrowser())
         {
-            StartGameRPC("Gym2", PlayroomKit.Me().id);
+            StartGameRPC("GameScene", PlayroomKit.Me().id);
             return;
         }
         if (PlayroomKit.IsHost())
         {
             Debug.Log("Calling RPC to start game!");
-            PlayroomKit.RpcCall("Start", "Gym2", PlayroomKit.RpcMode.ALL, StartGameConfirmCallback);
+            PlayroomKit.RpcCall("Start", "GameScene", PlayroomKit.RpcMode.ALL, StartGameConfirmCallback);
         }
         else 
             Debug.LogError("[ButtonStartGame] Hey, how did you get to this?");
